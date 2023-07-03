e1f7e10ede63bece7a6c830dcf64ed2b09985084
yasmen ahmed <130899829+yasmen310@users.noreply.github.com>
2023-07-03 11:08:45 -0700

    Add files via upload

---
+++ onlineTraining.js
@@ -0,0 +1,10 @@
+setDefault();
+function setDefault(){
+    document.getElementById("content").style.display="none";
+}
+function openList(){
+     document.getElementById("content").style.display="block";
+}
+function closeList(){
+    document.getElementById("content").style.display="none";
+}
